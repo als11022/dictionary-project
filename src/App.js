90a2ce510d520676be1066095e2510f8c6e1d9cd
als11022 <95195221+als11022@users.noreply.github.com>
2022-04-19 10:53:30 -0400

    Added search form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        Hello
+        Dictionary App
       </header>
+      <main>
+        <Dictionary />
+      </main>
+<footer className="App-footer">
+  Coded by Abby Szach
+</footer>
+      </div>
     </div>
   );
 }
